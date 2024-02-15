ebb11858eb318e416257ce1a259755bdef8eb183
Fernando Felicio <fernandofelicio.dev@gmail.com>
2024-02-15 09:21:45 -0300

    implementing PhysicalPerson Class

--- src/entities/PhysicalPerson.java
+++ src/entities/PhysicalPerson.java
@@ -1,5 +1,46 @@
 package entities;
 
 public class PhysicalPerson {
+	
+	private String name;
+	private Double annualIncome;
+	private Double healthExpenses;
+	
+	public PhysicalPerson() {
+		super();		
+	}
+	
+	public PhysicalPerson(String name, Double annualIncome, Double healthExpenses) {
+		super();
+		this.name = name;
+		this.annualIncome = annualIncome;
+		this.healthExpenses = healthExpenses;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getAnnualIncome() {
+		return annualIncome;
+	}
+
+	public void setAnnualIncome(Double annualIncome) {
+		this.annualIncome = annualIncome;
+	}
+
+	public Double getHealthExpenses() {
+		return healthExpenses;
+	}
+
+	public void setHealthExpenses(Double healthExpenses) {
+		this.healthExpenses = healthExpenses;
+	}
+	
+	
 
 }
